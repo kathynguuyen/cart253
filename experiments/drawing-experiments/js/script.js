22fd2016b2f4a951f7a6058d19e6cec7fd72c1fd
kathynguuyen <kathynguuyen@gmail.com>
2020-09-21 19:04:28 -0400

    Update script.js

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -9,6 +9,19 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+  background(200,200,200);
+
+
+  point(250,250);
+
+  rect(0,0,500,250);
+
+  stroke(100,255,0);
+  fill(127,0,200);
+  ellipse(100,100,100,100);
+
+
 
 }
 
