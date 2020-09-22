ab372ef1fd707bb54dd7011b5ef94360bbab8555
kathynguuyen <kathynguuyen@gmail.com>
2020-09-22 12:38:13 -0400

    Activity 3: Moving pictures

--- experiments/movingpicture-activity/js/script.js
+++ experiments/movingpicture-activity/js/script.js
@@ -4,33 +4,17 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-let backgroundShade = 0;
-
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  speed: 2,
-  fill: 255
-};
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500,500);
+
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
-  background(backgroundShade);
-  circle.x += circle.speed;
-  circle.x = constrain(circle.x,250,width);
-  fill(circle.fill);
-  ellipse(circle.x,circle.y,circle.size);
-
 
 }
