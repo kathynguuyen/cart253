206143979fcf5cb2dfcd7d2640363c25c25c1b3b
kathynguuyen <kathynguuyen@gmail.com>
2020-10-11 18:49:41 -0400

    P1: Started project 1 again

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -5,10 +5,18 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let titleScreen;
+
+function preload() {
+  titleScreen = loadImage('assets/images/titleScreen.png');
+}
+
+let state = `title`; // title, how to play, simulation, win, lose
+
 // setup()
-//
-// Description of setup() goes here.
 function setup() {
+
+  createCanvas(1000,500);
 
 }
 
@@ -16,5 +24,17 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(109,207,246);
 
+  if(state == `title`) {
+    title();
+  }
+
+}
+
+
+function title() {
+  push();
+  image(titleScreen,0,0,1000,500);
+  pop();
 }
