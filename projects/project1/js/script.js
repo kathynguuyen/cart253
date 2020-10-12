2d2dd1a401bfa8b310cd1b702a657ab87481ee54
kathynguuyen <kathynguuyen@gmail.com>
2020-10-12 19:25:06 -0400

    P1: Display food

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,84 +7,115 @@ Here is a description of this template p5 project.
 
 let titleScreenImg;
 let pigImg;
+let carrotImg;
+let cornImg;
+let appleImg;
 
 let pig = {
-  x : 450,
-  y : 400,
-  size : 50,
+  x: 450,
+  y: 400,
+  sizeWidth: 150,
+  sizeHeight: 100,
   vx: 0,
   vy: 0,
   speed: 5,
-  img: pigImg
-}
+  img: pigImg,
+};
+
+
+let carrot = {
+  x: 0,
+  y: 100,
+  sizeWidth: 150,
+  sizeHeight: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  img: carrotImg,
+};
+
+let apple = {
+  x: 0,
+  y: 200,
+  sizeWidth: 150,
+  sizeHeight: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  img: appleImg,
+};
+
+let corn = {
+  x: 0,
+  y: 150,
+  sizeWidth: 150,
+  sizeHeight: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  img: cornImg,
+};
+
+
+
 
 function preload() {
-  titleScreenImg = loadImage('assets/images/titleScreenImg.png');
-  pigImg = loadImage('assets/images/pig.png');
+  titleScreenImg = loadImage("assets/images/titleScreenImg.png");
+  pigImg = loadImage("assets/images/pig.png");
+  carrotImg = loadImage("assets/images/carrot.png");
+  cornImg = loadImage("assets/images/corn.png");
+  appleImg = loadImage("assets/images/apple.png");
 }
 
 let state = `title`; // title, how to play, simulation, win, lose
 
 // setup()
 function setup() {
-
-  createCanvas(1000,500);
-
-
+  createCanvas(1000, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(109,207,246);
+  background(109, 207, 246);
 
   //states
-  if(state === `title`) {
+  if (state === `title`) {
     title();
   }
 
-  if(state === `simulation`) {
+  if (state === `simulation`) {
     simulation();
   }
 
-  if(state === `howToPlay`) {
+  if (state === `howToPlay`) {
     howToPlay();
   }
-
 }
 
 function title() {
   push();
-  image(titleScreenImg,0,0,1000,500);
+  image(titleScreenImg, 0, 0, 1000, 500);
   pop();
 }
 
 function simulation() {
-  background(0);
+  background(109, 207, 246);
 
   handleInput();
   move();
   display();
-
-
-
-
 }
-
 
 function howToPlay() {
-  background(100,0,0);
+  background(100, 0, 0);
 }
 
-
-
-
 function mousePressed() {
-
   // press the play button
-  if(state === `title`) {
-    if(mouseX > 40 && mouseX < 250) {
+  if (state === `title`) {
+    if (mouseX > 40 && mouseX < 250) {
       if (mouseY > 400 && mouseY < 430) {
         state = `simulation`;
       }
@@ -92,51 +123,51 @@ function mousePressed() {
   }
 
   // press the how to play button
-  if(state === `title`) {
-    if(mouseX > 700 && mouseX < 880) {
+  if (state === `title`) {
+    if (mouseX > 700 && mouseX < 880) {
       if (mouseY > 350 && mouseY < 460) {
         state = `howToPlay`;
       }
     }
   }
-
 }
 
-
 function move() {
+  // move the pig
   pig.x = pig.x + pig.vx;
   pig.y = pig.y + pig.vy;
 }
 
 function display() {
+  // display food
+  image(appleImg, apple.x, apple.y, apple.sizeWidth, apple.sizeHeight);
+  image(cornImg, corn.x, corn.y, corn.sizeWidth, corn.sizeHeight);
+  image(carrotImg, carrot.x, carrot.y, carrot.sizeWidth, carrot.sizeHeight);
+
   // display the pig
-  image(pigImg,pig.x,pig.y,150,100);
+  image(pigImg, pig.x, pig.y, pig.sizeWidth, pig.sizeHeight);
+
 
 }
-
 
 // move the pig with arrow keys
 function handleInput() {
   push();
-  if(keyIsDown(LEFT_ARROW)) {
+  if (keyIsDown(LEFT_ARROW)) {
     pig.vx = -pig.speed;
-  }
-  else if(keyIsDown(RIGHT_ARROW)) {
+  } else if (keyIsDown(RIGHT_ARROW)) {
     pig.vx = pig.speed;
-  }
-  else {
+  } else {
     pig.vx = 0;
   }
 
-
-  if(keyIsDown(UP_ARROW)) {
+  if (keyIsDown(UP_ARROW)) {
     pig.vy = -pig.speed;
   } else if (keyIsDown(DOWN_ARROW)) {
     pig.vy = pig.speed;
   } else {
     pig.vy = 0;
   }
-
 
   pop();
 }
