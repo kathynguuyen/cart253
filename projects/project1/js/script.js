2bbb572bd8e0e33f0343b562185e357604ede9fe
kathynguuyen <kathynguuyen@gmail.com>
2020-10-12 20:32:10 -0400

    P1: Check if apple is bigger than the pig

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -14,14 +14,13 @@ let appleImg;
 let pig = {
   x: 450,
   y: 400,
-  sizeWidth: 150,
-  sizeHeight: 100,
+  sizeWidth: 100,
+  sizeHeight: 50,
   vx: 0,
   vy: 0,
   speed: 5,
   img: pigImg,
 };
-
 
 let carrot = {
   x: 0,
@@ -37,8 +36,8 @@ let carrot = {
 let apple = {
   x: 0,
   y: 200,
-  sizeWidth: 150,
-  sizeHeight: 100,
+  sizeWidth: 50,
+  sizeHeight: 50,
   vx: 0,
   vy: 0,
   speed: 5,
@@ -56,9 +55,6 @@ let corn = {
   img: cornImg,
 };
 
-
-
-
 function preload() {
   titleScreenImg = loadImage("assets/images/titleScreenImg.png");
   pigImg = loadImage("assets/images/pig.png");
@@ -72,6 +68,10 @@ let state = `title`; // title, how to play, simulation, win, lose
 // setup()
 function setup() {
   createCanvas(1000, 500);
+
+  // apple appears at a random spot in the canvas
+  apple.x = random(0, width);
+  apple.vy = apple.speed;
 }
 
 // draw()
@@ -92,6 +92,10 @@ function draw() {
   if (state === `howToPlay`) {
     howToPlay();
   }
+
+  if (state === `lose`) {
+    lose();
+  }
 }
 
 function title() {
@@ -100,9 +104,14 @@ function title() {
   pop();
 }
 
+function lose() {
+  background(100, 0, 0);
+}
+
 function simulation() {
   background(109, 207, 246);
 
+  checkSize();
   handleInput();
   move();
   display();
@@ -136,18 +145,32 @@ function move() {
   // move the pig
   pig.x = pig.x + pig.vx;
   pig.y = pig.y + pig.vy;
+
+  // move apple
+  //apple.x = apple.x + apple.vx;
+  //apple.y = apple.y + apple.vy;
 }
 
 function display() {
   // display food
-  image(appleImg, apple.x, apple.y, apple.sizeWidth, apple.sizeHeight);
   image(cornImg, corn.x, corn.y, corn.sizeWidth, corn.sizeHeight);
   image(carrotImg, carrot.x, carrot.y, carrot.sizeWidth, carrot.sizeHeight);
 
+  // spawns apple everytime and change the size
+  //let x = apple.x;
+  // (let i = 0; i < 3; i++) {
+  image(appleImg, apple.x, apple.y, apple.sizeWidth, apple.sizeHeight);
+  //  x = x + 40;
+  //  if (apple.y > width) {
+  //  apple.y = 0;
+  //  apple.sizeWidth = random(50, 150);
+  //  apple.sizeHeight = random(20, 100);
+  //  apple.x = random(0, height);
+  //}
+  //}
+
   // display the pig
   image(pigImg, pig.x, pig.y, pig.sizeWidth, pig.sizeHeight);
-
-
 }
 
 // move the pig with arrow keys
@@ -170,4 +193,22 @@ function handleInput() {
   }
 
   pop();
+}
+
+// check if a fruit is bigger than the pig
+function checkSize() {
+  let d1 = dist(pig.x, pig.y, apple.x, apple.y);
+  // if the fruit is bigger, the player loses
+  if (  d1 <  (pig.sizeWidth + pig.sizeHeight) / 10 +    (pig.sizeWidth + pig.sizeHeight) / 10   ){
+    if (  (pig.sizeWidth + pig.sizeHeight) / 2 <  (apple.sizeWidth + apple.sizeHeight) / 2  ) {
+      state = `lose`;
+    }
+  }
+  // if the fruit is smaller, the pig increases its size
+  if ( d1 <(pig.sizeWidth + pig.sizeHeight) / 10 +  (pig.sizeWidth + pig.sizeHeight) / 10  ) {
+    if ((pig.sizeWidth + pig.sizeHeight) / 2 > (apple.sizeWidth + apple.sizeHeight) / 2  ) {
+      pig.sizeWidth = pig.sizeWidth + 1;
+      pig.sizeHeight = pig.sizeHeight + 1;
+    }
+  }
 }
