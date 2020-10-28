46cd3f6c3a143076a4439e0c077dc5d9687a50d7
kathynguuyen <kathynguuyen@gmail.com>
2020-10-28 18:58:10 -0400

    E5: Added movement to the paddle and display it

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -1,15 +1,22 @@
-use "strict";
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth,windowHeight);
 
+  paddle = new Paddle(300,20);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+
+  background(0);
+
+  paddle.move();
+  paddle.display();
 
 }
