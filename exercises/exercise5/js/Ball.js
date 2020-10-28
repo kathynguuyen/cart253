0d63c3402335efed4b7bd13ce1e89e81c368b207
kathynguuyen <kathynguuyen@gmail.com>
2020-10-28 19:04:23 -0400

    E5: Display the ball and added bounce function

--- exercises/exercise5/js/Ball.js
+++ exercises/exercise5/js/Ball.js
@@ -22,6 +22,27 @@ class Ball {
   move() {
     this.vx = this.vx + this.ax;
     this.vy = this.vy + this.ay;
+
+    this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+    this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
+
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+  }
+
+  bounce() {
+    if(this.y + this.size/2 >= height) {
+      this.vy = -this.vy;
+      this.ay = 0;
+    }
+  }
+
+  display() {
+    push();
+    fill(255,50,50);
+    stroke(0);
+    ellipse(this.x, this.y, this.size);
+    pop();
   }
 
 }
