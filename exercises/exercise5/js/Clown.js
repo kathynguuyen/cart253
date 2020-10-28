782a653be54899498200b12754727ffbac576a8f
kathynguuyen <kathynguuyen@gmail.com>
2020-10-28 19:37:56 -0400

    E5: Added clown class

---
+++ exercises/exercise5/js/Clown.js
@@ -0,0 +1,71 @@
+class Clown {
+
+
+  constructor(x,y) {
+
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.ax = 0;
+    this.ay = 0;
+    this.maxSpeed = 5;
+    this.size = 50;
+    this.active = true;
+
+  }
+
+  // gravity of the ball
+  gravity(force) {
+    this.ay = this.ay + force;
+  }
+
+
+  // make the ball move
+  move() {
+    this.vx = this.vx + this.ax;
+    this.vy = this.vy + this.ay;
+
+    this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+    this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
+
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if (this.y - this.size/2 > height) {
+      this.active = false;
+    }
+  }
+
+
+  // make the ball bounce
+  bounce(paddle) {
+    if (this.x > paddle.x - paddle.width/2 &&
+        this.x < paddle.x + paddle.width/2 &&
+        this.y + this.size/2 > paddle.y - paddle.height/2 &&
+        this.y - this.size/2 < paddle.y + paddle.height/2) {
+
+        // bounce
+        let dx = this.x - paddle.x;
+        this.vx = this.vx + map(dx,-paddle.width/2,paddle.width/2,-2,2);
+
+        this.vy = -this.vy;
+        this.ay = 0;
+
+
+    }
+
+
+  }
+
+
+  // display the ball
+  display() {
+    push();
+    image(clownImg,this.x,this.y,this.size);
+    pop();
+  }
+
+
+
+}
--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+let clownImg;
+
+function preload() {
+
+  clownImg = loadImage('assets/images/clown.png');
+
+}
+
 
  let gravityForce = 0.0025;
 
@@ -7,6 +15,9 @@
 
  let balls = [];
  let numBalls = 3;
+
+ let clowns = [];
+ let numClowns = 3;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -19,6 +30,14 @@ function setup() {
     let ball = new Ball(x,y);
     balls.push(ball);
   }
+
+
+    for (let i = 0; i < numClowns; i++) {
+      let x = random(0,width);
+      let y = random(-400,-100);
+      let clown = new Clown(x,y);
+      clowns.push(clown);
+    }
 
 }
 
@@ -41,7 +60,20 @@ function draw() {
 
     }
 
+    for(let i = 0; i < clowns.length; i++) {
+      let clown = clowns[i];
+      if (clown.active) {
+
+        clown.gravity(gravityForce);
+        clown.move();
+        clown.bounce(paddle);
+        clown.display();
+
+      }
+
   }
+
+}
 
 }
 
