b8d8c473ec02e8e17967958afda6ae7993115ac0
kathynguuyen <kathynguuyen@gmail.com>
2020-11-04 22:08:15 -0500

    Prototype: Added coordinates to know help with determining where to click the shape

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -15,9 +15,19 @@ function draw() {
   ellipse(300,80,55,55);
   rect(100, 50, 55, 55);
 
+  text("X: "+mouseX, 0, height/4);
+  text("Y: "+mouseY, 0, height/2);
 
 }
 
-function mousePressed() {
+function mouseClicked() {
+  if(mouseX > 250 && mouseX < 450) {
+    if(mouseY > 70 && mouseY < 85)
+    display();
+  }
+}
 
+
+function display() {
+  ellipse(200,200,55,55);
 }
