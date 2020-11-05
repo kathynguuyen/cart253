97117d476f6c39c9bfc5d3aa3a0c0099c344ead5
kathynguuyen <kathynguuyen@gmail.com>
2020-11-04 22:23:50 -0500

    Prototype: Display shape when we click on the shape

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -3,6 +3,7 @@ This is a prototype of my game.
 Whenever we click the option selected, the item will appear on the screen.
 **/
 
+
 function setup() {
 
   createCanvas(1000,500);
@@ -11,23 +12,44 @@ function setup() {
 
 function draw() {
 
-  background(0);
+  background(255, 209, 220);
   ellipse(300,80,55,55);
   rect(100, 50, 55, 55);
 
+  // temporary coordinates to determine where to display the shapes
+  //coordinates();
+
+
+  mouseClick();
+
+
+}
+
+function mousePressed() {
+
+  // display the circle when we click the circle
+  if(mouseX > 272 && mouseX < 327) {
+    if(mouseY > 50 && mouseY < 108)
+    displayCircle();
+  }
+  else if (mouseX > 100 && mouseX < 156) {
+    if(mouseY > 50 && mouseY < 108)
+    displaySquare();
+  }
+
+
+}
+
+
+function displayCircle() {
+  ellipse(200,200,55,55);
+}
+
+function displaySquare() {
+  rect(200,200,55,55);
+}
+
+function coordinates() {
   text("X: "+mouseX, 0, height/4);
   text("Y: "+mouseY, 0, height/2);
-
-}
-
-function mouseClicked() {
-  if(mouseX > 250 && mouseX < 450) {
-    if(mouseY > 70 && mouseY < 85)
-    display();
-  }
-}
-
-
-function display() {
-  ellipse(200,200,55,55);
 }
