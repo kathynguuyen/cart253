f03ceadcd58e609e8b3622324dab3b8699ded5ec
kathynguuyen <kathynguuyen@gmail.com>
2020-11-05 14:05:57 -0500

    Prototype: Added reset button, in case user wants to change their shape

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -3,12 +3,16 @@ This is a prototype of my game.
 Whenever we click the option selected, the item will appear on the screen.
 **/
 
+
 let circleBoolean= false;
 let squareBoolean = false;
 
 function setup() {
 
   createCanvas(1000,500);
+  resetSketch();
+  var button = createButton("reset");
+  button.mousePressed(resetSketch);
 
 }
 
@@ -37,16 +41,20 @@ function mousePressed() {
       circleBoolean = true;
     }
   }
+
+  //display the square when we click the circle
   else if (mouseX > 100 && mouseX < 156) {
     if(mouseY > 50 && mouseY < 108) {
       if(circleBoolean === false) {
         displaySquare();
         squareBoolean = true;
+
       }
-    }
   }
 
 
+
+}
 }
 
 
@@ -61,4 +69,13 @@ function displaySquare() {
 function coordinates() {
   text("X: "+mouseX, 0, height/4);
   text("Y: "+mouseY, 0, height/2);
+}
+
+
+function resetSketch() {
+  background(255, 209, 220);
+  ellipse(300,80,55,55);
+  rect(100, 50, 55, 55);
+  circleBoolean = false;
+  squareBoolean = false;
 }
