1e874aa8e71dd7094c80fbd69ccf104ceeb07afa
kathynguuyen <kathynguuyen@gmail.com>
2020-11-11 01:22:18 -0500

    E6: Display spider

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -10,11 +10,27 @@ Just a spider
 let synth;
 let note = [`G4`,`C4`,`C4`,`C4`,`C5`,`D5`,`E4`,`E4`];
 
+let currentNote = 0;
+
+let spiderImg;
+
+function preload() {
+  spiderImg = loadImage('assets/images/spider.jpg');
+}
+
+let spider = {
+  x: 100,
+  y: 100
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+
+  createCanvas(500,500);
+  synth = new p5.PolySynth();
+
 
 }
 
@@ -22,5 +38,10 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+
+
+    imageMode(CENTER);
+    image(spiderImg,mouseX,mouseY, 100,100);
 
 }
