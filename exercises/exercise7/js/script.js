4ad03e4839ed2086eada4d1a8aeb7affbd4376b2
kathynguuyen <kathynguuyen@gmail.com>
2020-11-18 03:38:01 -0500

    E7: Added states

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -10,33 +10,40 @@ let squareBoolean = false;
 let titleScreenImg;
 
 
+let state = `title`; // title, simulation
+
+
 // Load images and sounds
 function preload() {
 
-  titleScreenImg = loadImage("assets/images/titleScreenImg.jpg");
+  titleScreenImg = loadImage("assets/images/titleScreen.jpg");
 
 }
 
 function setup() {
 
   createCanvas(1000,500);
-  resetSketch();
-  var button = createButton("reset");
-  button.mousePressed(resetSketch);
+
 
 }
 
 function draw() {
 
-  background(255, 209, 220);
-  ellipse(300,80,55,55);
-  rect(100, 50, 55, 55);
-
   // temporary coordinates to determine where to display the shapes
   //coordinates();
 
 
-  mouseClick();
+  // states
+  if(state === `title`) {
+    title();
+  }
+
+
+  if(state === `simulation`) {
+    simulation();
+  }
+
+  //mouseClick();
 
 
 }
@@ -81,6 +88,25 @@ function displaySquare() {
 function coordinates() {
   text("X: "+mouseX, 0, height/4);
   text("Y: "+mouseY, 0, height/2);
+}
+
+
+function title() {
+  push();
+  image(titleScreenImg, 0, 0, 1000, 500);
+  coordinates();
+  pop();
+}
+
+function simulation() {
+  background(255, 209, 220);
+  ellipse(300,80,55,55);
+  rect(100, 50, 55, 55);
+
+  resetSketch();
+  var button = createButton("reset");
+  button.mousePressed(resetSketch);
+
 }
 
 
